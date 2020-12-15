db3f7dcbace76864f8138cd01190a1e27ab814aa
Philip Mattsson <31846047+Shogun616@users.noreply.github.com>
2020-12-15 13:44:55 +0100

    Update enemyship.py

--- enemyship.py
+++ enemyship.py
@@ -4,8 +4,4 @@ from point import Point
 class Enemyship(Polygon):
     
      def __init__(self,name, width, height):
-
-        
-         super().__init__( points=[ Point(20,20), Point(-10,10), Point(30,15), Point(-20,-20) ] )
-
-
+     super().__init__( points=[ Point(20,20), Point(-10,10), Point(30,15), Point(-20,-20) ] )
