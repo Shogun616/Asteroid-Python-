c39a7c8f3c16b3763f48c215da187c0835ee0f34
Philip Mattsson <31846047+Shogun616@users.noreply.github.com>
2020-12-15 13:39:44 +0100

    Update spacerock2.py

--- spacerock2.py
+++ spacerock2.py
@@ -10,10 +10,5 @@ class Spacerock2(Polygon):
         Polygon.__init__(self, points=[Point(0,0), Point(13,-7), Point(20,0), Point(30,17), Point(30,30), Point(7,38), Point(0,20)], x=x, y=y, rotation=rotation)
 
         self.accelerate(0.8)
-
         self.rotate(random.randrange(360))
-
         self.angular_velocity = (random.randrange(0,190)/100)
-   
-
-
